9177130c8683827f0cda0c4d76c479e62db44510
thedon45 <92436587+thedon45@users.noreply.github.com>
2023-03-28 09:53:02 +0300

    this was my first portfolio

---
+++ js/style-switcher.js
@@ -0,0 +1,5 @@
+// toogle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
